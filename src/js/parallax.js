dda5adc348b3f620125a9945d6489b31b24aefc5
DanilRostov <daniil.rossy@gmail.com>
2018-04-14 10:16:54 +0300

    some

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -1,15 +1,84 @@
 $(window).on('scroll', function() {
 	const scrl = $(this).scrollTop();
 
-	$('.about-audit__planet1').css({
-		"transform" : `translate(0%, ${scrl / 7}%)`
+	$('.promo__planet1').css({
+		"transform" : `translate(0%, -${scrl / 17.5}%)`
 	});
 
-	$('.about-audit__planet2').css({
-		"transform" : `translate(0%, ${scrl / 2.75}%)`
+	$('.promo__planet2').css({
+		"transform" : `translate(0%, -${scrl / 1}%)`
 	});
 
-	$('.about-audit__planet3').css({
-		"transform" : `translate(0%, -${scrl / 20}%)`
+	$('.promo__planet3').css({
+		"transform" : `translate(0%, -${scrl / 1}%)`
 	});
+
+	$('.promo__planet4').css({
+		"transform" : `translate(0%, -${scrl / 1}%)`
+	});
+
+	$('.promo__planet5').css({
+		"transform" : `translate(0%, -${scrl / 1}%)`
+	});
+
+	$('.reviews__planet').css({
+		"transform" : `translate(0%, -${scrl / 40}%)`
+	});
+
+	$('.frontend-promo__planet').css({
+		"transform" : `translate(0%, -${scrl / 3}%)`
+	});
+
+	$('.frontend-promo__planet2').css({
+		"transform" : `translate(0%, -${scrl / 2}%)`
+	});
+
+	$('.frontend-promo__planet3').css({
+		"transform" : `translate(0%, -${scrl / 3}%)`
+	});
+
+	$('.audit-promo__planet1').css({
+		"transform" : `translate(0%, -${scrl / 3}%)`
+	});
+
+	$('.audit-promo__planet2').css({
+		"transform" : `translate(0%, -${scrl / 2}%)`
+	});
+
+	$('.audit-promo__planet3').css({
+		"transform" : `translate(0%, -${scrl / 3}%)`
+	});
+
+	$('.promo-blog__planet1').css({
+		"transform" : `translate(0%, -${scrl / 10}%)`
+	});
+
+	$('.promo-blog__planet2').css({
+		"transform" : `translate(0%, -${scrl / 3}%)`
+	});
+
+	$('.promo-blog__planet3').css({
+		"transform" : `translate(0%, -${scrl / 1}%)`
+	});
+
+	$('.promo-blog__planet4').css({
+		"transform" : `translate(0%, -${scrl / 4}%)`
+	});
+
+	$('.promo-blog__planet5').css({
+		"transform" : `translate(0%, -${scrl / 2}%)`
+	});
+
+	$('.article-promo__planet1').css({
+		"transform" : `translate(0%, -${scrl / 15}%)`
+	});
+
+	$('.article-promo__planet2').css({
+		"transform" : `translate(0%, -${scrl / 2}%)`
+	});
+
+	$('.article-promo__planet3').css({
+		"transform" : `translate(0%, -${scrl / 5}%)`
+	});
+
 });
